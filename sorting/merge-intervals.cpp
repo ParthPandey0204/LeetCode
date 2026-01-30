b47be64988025256ce1c9e12b74e4f4a96773dc8
Parth Pandey <parth.pandey80045@gmail.com>
2026-01-30 22:25:23 +0530

    LC-merge-intervals [Runtime: 3 ms; Memory: 23.8 MB]

--- sorting/merge-intervals.cpp
+++ sorting/merge-intervals.cpp
@@ -1,18 +1,33 @@
+#include <bits/stdc++.h>
+using namespace std;
+
 class Solution {
 public:
     vector<vector<int>> merge(vector<vector<int>>& intervals) {
-        int n = intervals.size();
-        sort(intervals.begin(),intervals.end());
+
+        sort(intervals.begin(), intervals.end());
+
         vector<vector<int>> ans;
-         for(int i=0; i<n;i++){
-            if(ans.empty() || intervals[i][0]>ans.back()[1]){
-                ans.push_back(intervals[i]);
+
+        int n = intervals.size();
+        for (int i = 0; i < n; ) {
+
+            int start = intervals[i][0];
+            int end = intervals[i][1];
+
+            int j = i + 1;
+            while (j < n && intervals[j][0] <= end) {
+               
+                end = max(end, intervals[j][1]);
+                j++;
             }
-            else{
-                ans.back()[1] = max(ans.back()[1],intervals[i][1]);
-            }
-         }
-         return ans;
+
+            ans.push_back({start, end});
+
         
+            i = j;
+        }
+
+        return ans;
     }
 };
