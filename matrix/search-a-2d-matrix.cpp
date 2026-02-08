96f11765d460520c274a888a953e69df3377a87c
Parth Pandey <parth.pandey80045@gmail.com>
2026-02-08 22:38:47 +0530

    LC-search-a-2d-matrix [Runtime: N/A; Memory: N/A]

--- matrix/search-a-2d-matrix.cpp
+++ matrix/search-a-2d-matrix.cpp
@@ -3,25 +3,22 @@ public:
     bool searchMatrix(vector<vector<int>>& matrix, int target) {
         int m = matrix.size();
         int n = matrix[0].size();
-
+        
         int low = 0, high = m * n - 1;
-
+        
         while (low <= high) {
             int mid = low + (high - low) / 2;
-
             int row = mid / n;
             int col = mid % n;
-
-            int value = matrix[row][col];
-
-            if (value == target)
-                return true;
-            else if (value < target)
-                low = mid + 1;
-            else
-                high = mid - 1;
+            
+            int val = matrix[row][col];
+            
+            if (val == target) return true;
+            else if (val < target) low = mid + 1;
+            else high = mid - 1;
         }
-
+        
         return false;
     }
 };
+S
