872fcefef778590e1fc2221a78bad5bcd0c3a5ea
Parth Pandey <parth.pandey80045@gmail.com>
2026-02-23 17:34:59 +0530

    LC-sort-characters-by-frequency [Runtime: 0 ms; Memory: 11.2 MB]

--- sorting/sort-characters-by-frequency.cpp
+++ sorting/sort-characters-by-frequency.cpp
@@ -1,21 +1,20 @@
 class Solution {
 public:
     string frequencySort(string s) {
-     map<char,int>Counter;
-     for(int i= 0;i<s.size();i++){
-        Counter[s[i]]++;
-     }
-     priority_queue<pair<int,char>>pq;
-     for (auto p : Counter) {
-    pq.push({p.second, p.first});
-}
-     string res = "";
-     while(!pq.empty()){
-        pair<int,char>top = pq.top();
-        pq.pop();
-        res.append(top.first,top.second);
-     }
-     return res;
+        unordered_map<char,int> Counter;
+        for(char c : s){
+            Counter[c]++;
+        }
+        priority_queue<pair<int , char>>pq;
+        for(auto p : Counter){
+            pq.push({p.second , p.first});
+        }
+        string res = "";
+        while(!pq.empty()){
+            pair<int, char>top = pq.top();
+            pq.pop();
+            res.append(top.first , top.second);
+        }
+        return res;
     }
-
 };
