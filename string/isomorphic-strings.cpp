3161b9185d6ac6de7b9919f433d4ee782d73b5c2
Parth Pandey <parth.pandey80045@gmail.com>
2026-02-23 16:01:22 +0530

    LC-isomorphic-strings [Runtime: 0 ms; Memory: 9.4 MB]

--- string/isomorphic-strings.cpp
+++ string/isomorphic-strings.cpp
@@ -1,25 +1,16 @@
 class Solution {
 public:
     bool isIsomorphic(string s, string t) {
-        if (s.size() != t.size()) return false;
-        
-        unordered_map<char, char> map_s_to_t;
-        unordered_map<char, char> map_t_to_s;
-        
-        for (int i = 0; i < s.size(); i++) {
-            if (map_s_to_t.find(s[i]) != map_s_to_t.end()) {
-                if (map_s_to_t[s[i]] != t[i]) return false;
-            } else {
-                map_s_to_t[s[i]] = t[i];
-            }
+       if(s.size() != t.size())return false;
+       int map1[256] = {0};
+       int map2[256] = {0};
+         for(int i =0; i< s.size() ; i++){
+            if(map1[s[i]]!= map2[t[i]])
+            return false ;
+            map1[s[i]] = i+1;
+            map2[t[i]] = i+1;
 
-            if (map_t_to_s.find(t[i]) != map_t_to_s.end()) {
-                if (map_t_to_s[t[i]] != s[i]) return false;
-            } else {
-                map_t_to_s[t[i]] = s[i];
-            }
-        }
-        
-        return true;
+         }
+         return true;
     }
 };
