1388d78249b43e27bc8600f79a283b880d8cb580
Parth Pandey <parth.pandey80045@gmail.com>
2026-02-24 10:47:49 +0530

    LC-string-to-integer-atoi [Runtime: 3 ms; Memory: 9.3 MB]

--- string/string-to-integer-atoi.cpp
+++ string/string-to-integer-atoi.cpp
@@ -1,29 +1,27 @@
 class Solution {
 public:
     int myAtoi(string s) {
-        if(s.length()==0)return 0;
-       int i=0;
-       while(i<s.size() && s[i]==' '){
+        int i = 0 ; 
+        int n = s.length();
+        while(i < n && s[i]==' ') i++;
+        int sign = 1;
+        if( i < n && (s[i] == '+' || s[i]== '-')){
+            if(s[i] == '-')sign = -1;
             i++;
-       }
-        s=s.substr(i);
-        int sign =1;
-        long long num = 0;
-        if(s[0]=='-')sign = -1; 
-        if(s[0]=='+'||s[0]=='-'){
-            i=1;
         }
-        else {
-            i=0;
-            }
+        long result = solve(s , i , 0 , sign);
+        return (int)(sign * result);
+    }
+    long solve(string &s, int i , long result , int sign){
+        if( i >= s.length() || !isdigit(s[i]))
+        return result;
 
-        while(i<s.size()){
-            if(s[0]==' ' || !isdigit(s[i]))break;
-            num = num*10 + (s[i]-'0');
-            if(sign == -1 && -1*num < INT_MIN)return INT_MIN;
-            if(sign == 1 && 1*num > INT_MAX)return INT_MAX;
-            i++;
-        }
-        return sign*num;
+        int digit = s[i] - '0';
+         if (result > (INT_MAX - digit) / 10)
+            return sign == 1 ? INT_MAX : -(long)INT_MIN;
+        
+        result = result * 10 + digit;
+        
+        return solve(s, i + 1, result, sign);
     }
 };
