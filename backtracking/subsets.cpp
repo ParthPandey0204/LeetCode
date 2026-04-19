d0482727379f2f1050e85f21e410569d51097dbe
Parth Pandey <parth.pandey80045@gmail.com>
2026-04-19 23:34:07 +0530

    LC-subsets [Runtime: 3 ms; Memory: 10 MB]

---
+++ backtracking/subsets.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    vector<vector<int>> subsets(vector<int>& nums) {
+        int n = nums.size();
+        vector<vector<int>> result;
+
+        int total = 1 << n; 
+
+        for (int mask = 0; mask < total; mask++) {
+            vector<int> subset;
+
+            for (int i = 0; i < n; i++) {
+                if (mask & (1 << i)) {
+                    subset.push_back(nums[i]);
+                }
+            }
+
+            result.push_back(subset);
+        }
+
+        return result;
+    }
+};
