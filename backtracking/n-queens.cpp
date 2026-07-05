24c028d2e8f883bf15095dfd98ea4563e4327e45
Parth Pandey <parth.pandey80045@gmail.com>
2026-07-05 21:55:58 +0530

    LC-n-queens [Runtime: N/A; Memory: N/A]

--- backtracking/n-queens.cpp
+++ backtracking/n-queens.cpp
@@ -1,45 +1,40 @@
 class Solution {
 public:
+    vector<vector<string>> ans;
+
     void solve(int row, int n, vector<string>& board,
-               vector<vector<string>>& ans,
-               vector<int>& col,
-               vector<int>& diag1,
-               vector<int>& diag2) {
-        
+               vector<int>& col, vector<int>& diag1, vector<int>& diag2) {
         if (row == n) {
             ans.push_back(board);
             return;
         }
 
         for (int c = 0; c < n; c++) {
-            if (col[c] == 0 && 
-                diag1[row + c] == 0 && 
-                diag2[row - c + n - 1] == 0) {
-                
-                board[row][c] = 'Q';
-                col[c] = 1;
-                diag1[row + c] = 1;
-                diag2[row - c + n - 1] = 1;
+            if (col[c] || diag1[row - c + n - 1] || diag2[row + c])
+                continue;
 
-                solve(row + 1, n, board, ans, col, diag1, diag2);
+            board[row][c] = 'Q';
+            col[c] = 1;
+            diag1[row - c + n - 1] = 1;
+            diag2[row + c] = 1;
 
-                board[row][c] = '.';
-                col[c] = 0;
-                diag1[row + c] = 0;
-                diag2[row - c + n - 1] = 0;
-            }
+            solve(row + 1, n, board, col, diag1, diag2);
+
+            board[row][c] = '.';
+            col[c] = 0;
+            diag1[row - c + n - 1] = 0;
+            diag2[row + c] = 0;
         }
     }
 
     vector<vector<string>> solveNQueens(int n) {
-        vector<vector<string>> ans;
         vector<string> board(n, string(n, '.'));
-
         vector<int> col(n, 0);
-        vector<int> diag1(2*n - 1, 0);
-        vector<int> diag2(2*n - 1, 0);
+        vector<int> diag1(2 * n - 1, 0);
+        vector<int> diag2(2 * n - 1, 0);
 
-        solve(0, n, board, ans, col, diag1, diag2);
+        solve(0, n, board, col, diag1, diag2);
+
         return ans;
     }
 };
