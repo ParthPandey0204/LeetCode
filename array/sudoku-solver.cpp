a7c33205f24e99c3883bb4a3b08f626e39722d20
Parth Pandey <parth.pandey80045@gmail.com>
2026-07-05 21:57:24 +0530

    LC-sudoku-solver [Runtime: N/A; Memory: N/A]

--- array/sudoku-solver.cpp
+++ array/sudoku-solver.cpp
@@ -1,63 +1,63 @@
 class Solution {
 public:
     bool solve(vector<vector<char>>& board,
-               vector<vector<int>>& row,
-               vector<vector<int>>& col,
-               vector<vector<int>>& box) {
-        
-        for (int r = 0; r < 9; r++) {
-            for (int c = 0; c < 9; c++) {
-                
-                if (board[r][c] == '.') {
-                    
-                    for (int num = 1; num <= 9; num++) {
-                        
-                        int boxIndex = (r / 3) * 3 + (c / 3);
-                        
-                        if (row[r][num] == 0 &&
-                            col[c][num] == 0 &&
-                            box[boxIndex][num] == 0) {
-                            
-                            board[r][c] = num + '0';
-                            row[r][num] = 1;
-                            col[c][num] = 1;
-                            box[boxIndex][num] = 1;
-                            
-                            if (solve(board, row, col, box))
-                                return true;
-                            
-                            board[r][c] = '.';
-                            row[r][num] = 0;
-                            col[c][num] = 0;
-                            box[boxIndex][num] = 0;
-                        }
+               vector<vector<int>>& rows,
+               vector<vector<int>>& cols,
+               vector<vector<int>>& boxes) {
+
+        for (int i = 0; i < 9; i++) {
+            for (int j = 0; j < 9; j++) {
+
+                if (board[i][j] == '.') {
+
+                    int box = (i / 3) * 3 + (j / 3);
+
+                    for (char ch = '1'; ch <= '9'; ch++) {
+                        int num = ch - '1';
+
+                        if (rows[i][num] || cols[j][num] || boxes[box][num])
+                            continue;
+
+                        board[i][j] = ch;
+                        rows[i][num] = 1;
+                        cols[j][num] = 1;
+                        boxes[box][num] = 1;
+
+                        if (solve(board, rows, cols, boxes))
+                            return true;
+
+                        board[i][j] = '.';
+                        rows[i][num] = 0;
+                        cols[j][num] = 0;
+                        boxes[box][num] = 0;
                     }
-                    
+
                     return false;
                 }
             }
         }
-        return true; 
+
+        return true;
     }
 
     void solveSudoku(vector<vector<char>>& board) {
-        
-        vector<vector<int>> row(9, vector<int>(10, 0));
-        vector<vector<int>> col(9, vector<int>(10, 0));
-        vector<vector<int>> box(9, vector<int>(10, 0));
-    
-        for (int r = 0; r < 9; r++) {
-            for (int c = 0; c < 9; c++) {
-                if (board[r][c] != '.') {
-                    int num = board[r][c] - '0';
-                    int boxIndex = (r / 3) * 3 + (c / 3);
-                    row[r][num] = 1;
-                    col[c][num] = 1;
-                    box[boxIndex][num] = 1;
+        vector<vector<int>> rows(9, vector<int>(9, 0));
+        vector<vector<int>> cols(9, vector<int>(9, 0));
+        vector<vector<int>> boxes(9, vector<int>(9, 0));
+
+        for (int i = 0; i < 9; i++) {
+            for (int j = 0; j < 9; j++) {
+                if (board[i][j] != '.') {
+                    int num = board[i][j] - '1';
+                    int box = (i / 3) * 3 + (j / 3);
+
+                    rows[i][num] = 1;
+                    cols[j][num] = 1;
+                    boxes[box][num] = 1;
                 }
             }
         }
-        
-        solve(board, row, col, box);
+
+        solve(board, rows, cols, boxes);
     }
 };
