22502dba0fb48186f8631ef195fde776a63ca30d
Parth Pandey <parth.pandey80045@gmail.com>
2026-07-23 23:57:33 +0530

    LC-median-of-two-sorted-arrays [Runtime: N/A; Memory: N/A]

--- binary-search/median-of-two-sorted-arrays.cpp
+++ binary-search/median-of-two-sorted-arrays.cpp
@@ -2,28 +2,37 @@ class Solution {
 public:
     double findMedianSortedArrays(vector<int>& nums1, vector<int>& nums2) {
         int n1 = nums1.size(), n2 = nums2.size();
-    if (n1 > n2) return  findMedianSortedArrays(nums2 , nums1) ;
 
-    int n = n1 + n2; 
-    int left = (n1 + n2 + 1) / 2;
-    int low = 0, high = n1;
-    while (low <= high) {
-        int mid1 = (low + high) >> 1;
-        int mid2 = left - mid1;
-        int l1 = INT_MIN, l2 = INT_MIN;
-        int r1 = INT_MAX, r2 = INT_MAX;
-        if (mid1 < n1) r1 = nums1[mid1];
-        if (mid2 < n2) r2 = nums2[mid2];
-        if (mid1 - 1 >= 0) l1 = nums1[mid1 - 1];
-        if (mid2 - 1 >= 0) l2 = nums2[mid2 - 1];
+   
+        if (n1 > n2)
+            return findMedianSortedArrays(nums2, nums1);
 
-        if (l1 <= r2 && l2 <= r1) {
-            if (n % 2 == 1) return max(l1, l2);
-            else return ((double)(max(l1, l2) + min(r1, r2))) / 2.0;
+        int low = 0, high = n1;
+
+        while (low <= high) {
+            int cut1 = (low + high) / 2;
+            int cut2 = (n1 + n2 + 1) / 2 - cut1;
+
+            int l1 = (cut1 == 0) ? INT_MIN : nums1[cut1 - 1];
+            int l2 = (cut2 == 0) ? INT_MIN : nums2[cut2 - 1];
+
+            int r1 = (cut1 == n1) ? INT_MAX : nums1[cut1];
+            int r2 = (cut2 == n2) ? INT_MAX : nums2[cut2];
+
+            if (l1 <= r2 && l2 <= r1) {
+                if ((n1 + n2) % 2 == 0)
+                    return (max(l1, l2) + min(r1, r2)) / 2.0;
+                else
+                    return max(l1, l2);
+            }
+            else if (l1 > r2) {
+                high = cut1 - 1;
+            }
+            else {
+                low = cut1 + 1;
+            }
         }
-        else if (l1 > r2) high = mid1 - 1;
-        else low = mid1 + 1;
-    }
-    return 0;
+
+        return 0.0;
     }
 };
