f2a7aa18943b7e96557948d38a0ade9e039cf382
Parth Pandey <parth.pandey80045@gmail.com>
2026-09-01 21:23:07 +0530

    LC-implement-stack-using-queues [Runtime: N/A; Memory: N/A]

--- design/implement-stack-using-queues.cpp
+++ design/implement-stack-using-queues.cpp
@@ -1,27 +1,21 @@
 class MyStack {
+    queue<int>q;
 public:
-    queue<int> q;
-
     MyStack() {
-        
     }
-    
     void push(int x) {
+        int s = q.size();
         q.push(x);
-        
-        int size = q.size();
-        
-        // rotate previous elements
-        for(int i = 0; i < size - 1; i++) {
-            q.push(q.front());
-            q.pop();
+        for( int i = 0 ; i < s ; i++){
+              q.push(q.front());
+              q.pop();
         }
     }
     
     int pop() {
-        int val = q.front();
+        int x = q.front();
         q.pop();
-        return val;
+        return x;
     }
     
     int top() {
@@ -29,6 +23,16 @@ public:
     }
     
     bool empty() {
-        return q.empty();
+        if(q.size() == 0) return true ;
+        else return false;
     }
 };
+
+/**
+ * Your MyStack object will be instantiated and called as such:
+ * MyStack* obj = new MyStack();
+ * obj->push(x);
+ * int param_2 = obj->pop();
+ * int param_3 = obj->top();
+ * bool param_4 = obj->empty();
+ */
