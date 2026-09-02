b612e3afac87f4f7c3a28575457cea3ec6e81599
Parth Pandey <parth.pandey80045@gmail.com>
2026-09-02 17:44:29 +0530

    LC-palindrome-number [Runtime: N/A; Memory: N/A]

--- math/palindrome-number.cpp
+++ math/palindrome-number.cpp
@@ -1,18 +1,18 @@
 class Solution {
 public:
     bool isPalindrome(int x) {
-        if (x < 0) {
+        if (x < 0)
             return false;
-        }
-        long long reversed = 0;
-        long long temp = x;
 
-        while (temp != 0) {
-            int digit = temp % 10;
-            reversed = reversed * 10 + digit;
-            temp /= 10;
+        int original = x;
+        int rev = 0;
+
+        while (x != 0) {
+            int digit = x % 10;
+            rev = rev * 10 + digit;
+            x /= 10;
         }
-        return (reversed == x);
+
+        return original == rev;
     }
 };
-
