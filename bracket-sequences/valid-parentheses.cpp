610ee40b6289ca8d8a4054bed8f81f8a08a1c1f3
Parth Pandey <parth.pandey80045@gmail.com>
2026-09-02 17:31:07 +0530

    LC-valid-parentheses [Runtime: N/A; Memory: N/A]

---
+++ bracket-sequences/valid-parentheses.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    bool isValid(string s) {
+        stack<char>a;
+        int n = s.size();
+        for(int i=0;i<n;i++){
+            if(s[i]=='(' || s[i]=='{' || s[i]=='['){
+                a.push(s[i]);
+            }
+        else{ 
+            if(a.empty()) return false;
+        char ch = a.top();
+        a.pop();
+        if(!((s[i] == ')' && ch == '(') || (s[i] == '}' && ch == '{') || (s[i] == ']' && ch == '[')))
+         return false;
+        }  
+    }
+    return true;
+    }
+};
